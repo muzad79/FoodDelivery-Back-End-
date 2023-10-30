3c15406b6f14176096ef7301f1a51bd704318270
Muzamil Adnan <muzamil.adnan@gildwaretechnologies.com>
2023-10-30 15:09:42 +0530

    dd

--- controllers/foodControllers.js
+++ controllers/foodControllers.js
@@ -1,11 +1,12 @@
 const mongoose=require('mongoose')
 
-async function getFoodItems(req,res){  
+async function getFoodItems(req,res){ 
+    try{ 
     const fetchedFoodData =  mongoose.connection.db.collection("FoodItems")
     let foodData = await fetchedFoodData.find().toArray()
     const fetchedCategoryData =mongoose.connection.db.collection("FoodCategory")
     let foodCategory = await fetchedCategoryData.find().toArray()
-    try{
+    
         res.status(200).json({foodData:foodData,
         foodCategory:foodCategory})
 
